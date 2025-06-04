3b1f2775388d1fb6c12a1f9d860ee0b13184aa0d
Nahim <nahimmartinez03@gmail.com>
2025-06-04 09:51:21 -0300

    feature: saque indicador de slide de carrusel

--- app/Views/front/principal.php
+++ app/Views/front/principal.php
@@ -15,26 +15,7 @@ if ($msg): ?>
     data-bs-ride="carousel"
     data-bs-interval="5000"
   >
-    <!-- Indicadores -->
-    <div class="carousel-indicators">
-      <button
-        type="button"
-        data-bs-target="#mainCarousel"
-        data-bs-slide-to="0"
-        class="active"
-      ></button>
-      <button
-        type="button"
-        data-bs-target="#mainCarousel"
-        data-bs-slide-to="1"
-      ></button>
-      <button
-        type="button"
-        data-bs-target="#mainCarousel"
-        data-bs-slide-to="2"
-      ></button>
-    </div>
-
+  
     <!-- Slides -->
     <div class="carousel-inner">
       <!-- Slide 1 (Pesca) -->
