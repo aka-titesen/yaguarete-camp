6b17f1320c21a088b18a1ce21a9fd0d14208c93f
aka-titesen <zgf.nicolas@gmail.com>
2025-06-19 16:00:02 -0300

    refactor: agregue la x para cerrar el carrito de compras

--- app/Views/front/layouts/header.php
+++ app/Views/front/layouts/header.php
@@ -11,8 +11,8 @@
   <link href="<?= base_url('assets/fontawesome/css/all.min.css') ?>" rel="stylesheet" />
   <link href="https://fonts.googleapis.com/css2?family=Kaushan+Script&family=Caveat:wght@400;700&display=swap"
     rel="stylesheet">
+  <link rel="stylesheet" href="<?= base_url('assets/css/carrito-x-dark.css') ?>">
 </head>
 
 <body>
   <header>
-    
